75383832915e4eda9d7113f24ca2818efa5b33ed
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2023-07-26 12:01:34 -0500

    initial commit

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,18 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+  let pageTitle = 'Good Books';
+  let book1 =
+    'https://images-na.ssl-images-amazon.com/images/I/81+ZyXVB6lL._AC_UL127_SR127,127_.jpg';
+  let book2 =
+    'https://images-na.ssl-images-amazon.com/images/I/61jLcNyGeoL._AC_UL127_SR127,127_.jpg';
+  let book3 =
+    'https://images-na.ssl-images-amazon.com/images/I/710tAfTSp3L._AC_UL127_SR127,127_.jpg';
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
+  return (
+    <div>
+      <h3>{pageTitle}</h3>
+      <img src={book1} alt="The World Atlas of Coffee" />
+      <img src={book2} alt="Deliverance Prayers" />
+      <img src={book3} alt="Hunger Games" />
+    </div>
+  );
 }
